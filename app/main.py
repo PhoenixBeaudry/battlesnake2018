74918fb682125a5abf1ab09c2679987715758e95
greylogo <36058382+greylogo@users.noreply.github.com>
2018-03-02 00:05:16 -0800

    s

--- app/main.py
+++ app/main.py
@@ -170,11 +170,11 @@ def checkArray(array, location, types):
 #if the move was submitted
 def dest(move, current_location):
 	result={
-		'up': lambda x,y: [x,y-1]
-		'down': lambda x,y: [x,y+1]
-		'left': lambda x,y: [x-1,y]
+		'up': lambda x,y: [x,y-1],
+		'down': lambda x,y: [x,y+1],
+		'left': lambda x,y: [x-1,y],
 		'right': lambda x,y: [x+1,y]
-	}[move](current_location[0], current location[1])
+	}[move](current_location)
 	return result
 
 #returns false if the proposed move places us on a board wall
